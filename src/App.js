81c3fd10629c7b11696ff2a1dbadfb99fe48e74a
Jorge <jorgediazok@gmail.com>
2020-10-25 02:10:24 -0300

    countries and setcountries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 
 function App() {
+  const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+
   return (
-    <div className="App">
-      <h1>first commit</h1>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
