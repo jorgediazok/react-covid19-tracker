baf57ba5d8e0a54d098cc52f1ebf1a8ff2a5e932
Jorge <jorgediazok@gmail.com>
2020-10-25 01:27:00 -0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDJ0SIQo-ntk0BY2wjPC4IxzB4lMjd_3TM',
+  authDomain: 'covid-19-tracker-369c8.firebaseapp.com',
+  databaseURL: 'https://covid-19-tracker-369c8.firebaseio.com',
+  projectId: 'covid-19-tracker-369c8',
+  storageBucket: 'covid-19-tracker-369c8.appspot.com',
+  messagingSenderId: '1002639969895',
+  appId: '1:1002639969895:web:fef633e1e2f9d723c74ec2',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
