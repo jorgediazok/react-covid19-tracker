7e3962aaa45e45afb176542bf1343d13e6dd7ac7
Jorge <jorgediazok@gmail.com>
2020-10-26 17:29:48 -0300

    now added the flags

--- src/util.js
+++ src/util.js
@@ -40,7 +40,24 @@ export const showDataOnMap = (data, casesType = 'cases') =>
         Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
       }>
       <Popup>
-        <h1>I am a popup</h1>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{
+              backgroundImage: `url(${country.countryInfo.flag})`,
+            }}
+          />
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format('0,0')}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format('0,0')}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format('0,0')}
+          </div>
+        </div>
       </Popup>
     </Circle>
   ));
