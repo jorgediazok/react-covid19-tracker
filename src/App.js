02610bb69c68900daa716e69f56653788a9fbe01
Jorge <jorgediazok@gmail.com>
2020-10-26 15:17:08 -0300

    map showing up

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ function App() {
   const [country, setCountry] = useState(['worldwide']);
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all?yesterday=true')
@@ -99,7 +101,7 @@ function App() {
           />
         </div>
 
-        <Map />
+        <Map center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className="app__right">
         <CardContent>
--- src/components/Map.js
+++ src/components/Map.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Map as LeafletMap, TileLayer } from 'react-leaflet';
 import '../styles/Map.css';
 
-function Map() {
+function Map({ center, zoom }) {
   return (
     <div className="map">
-      <LeafletMap>
+      <LeafletMap center={center} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap></a> contributors'
